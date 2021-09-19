81bcbd25b4dc4fe46eafee5ab6bb0ae5af949894
Command <maoxs2@gmail.com>
2021-09-19 20:25:15 +0800

    enhance the web test example

--- testBrowser/dart_application_test/web/main.dart
+++ testBrowser/dart_application_test/web/main.dart
@@ -4,8 +4,19 @@ import 'package:fast_base58/fast_base58.dart';
 
 void main() {
   var input = querySelector('#input');
-  input!.onKeyUp.listen((e) {
-    var raw = Base58Decode((input as InputElement).value!);
+  var enc = querySelector('#encode');
+  var dec = querySelector('#decode');
+  enc?.onClick.listen((e) {
+    var raw = utf8.encode((input as TextAreaElement).value!);
+
+    querySelector('#hex')?.text = List.generate(
+            raw.length, (index) => raw[index].toRadixString(16).padLeft(2, '0'))
+        .join();
+    querySelector('#output')?.text = Base58Encode(raw);
+  });
+
+  dec?.onClick.listen((e) {
+    var raw = Base58Decode((input as TextAreaElement).value!);
 
     querySelector('#hex')?.text = List.generate(
             raw.length, (index) => raw[index].toRadixString(16).padLeft(2, '0'))
